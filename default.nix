d98a6b657a4f8ea053a1e5882fcb58d625ac11ec
Shane Sveller <shanesveller@gmail.com>
2020-02-27 22:23:05 -0600

    Test build with nixpkgs upstream cached rustc

--- default.nix
+++ default.nix
@@ -1,9 +1,7 @@
 { pkgs ? import ./nix { } }:
 
 let
-  rustStable = pkgs.rustChannels.stable.rust;
-  buildRustCrate = pkgs.buildRustCrate.override { rustc = rustStable; };
-  cargo_nix = pkgs.callPackage ./Cargo.nix { inherit buildRustCrate; };
+  cargo_nix = pkgs.callPackage ./Cargo.nix { };
   actionstar = cargo_nix.rootCrate.build;
 
 in { inherit actionstar; }
