bb9dba199316da33501ffbf168b2088f89cdb315
Shane Sveller <shanesveller@gmail.com>
2020-02-27 21:25:01 -0600

    Add nixpkgs-mozilla overlay to Niv default context

--- nix/default.nix
+++ nix/default.nix
@@ -1,11 +1,12 @@
 { sources ? import ./sources.nix }:
 with {
+  moz_overlay = import sources.nixpkgs-mozilla;
   overlay = _: pkgs: {
     lorri = import sources.lorri { inherit pkgs; };
     niv = import sources.niv { };
   };
 };
 import sources.nixpkgs {
-  overlays = [ overlay ];
+  overlays = [ overlay moz_overlay ];
   config = { };
 }
