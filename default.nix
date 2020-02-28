b8a5cfabcac6fc1cd21a8db84f2c53af13a147fd
Shane Sveller <shanesveller@gmail.com>
2020-02-27 23:48:00 -0600

    fixup! WIP

--- default.nix
+++ default.nix
@@ -1,7 +1,4 @@
-{ crossSystem ? null, system ? builtins.currentSystem, pkgs ? import ./nix {
-  inherit system;
-  crossSystem = { config = crossSystem; };
-} }:
+{ system ? builtins.currentSystem, pkgs ? import ./nix { inherit system; } }:
 
 let
   # https://www.reddit.com/r/NixOS/comments/f0yi3b/how_to_build_a_simple_static_rust_binary_using/fh2asml/
@@ -9,8 +6,11 @@ let
     # extensions = [ "rust-src" ];
     targets = [ "x86_64-unknown-linux-musl" ];
   };
-  buildRustCrate = pkgs.buildRustCrate.override { rustc = rust; };
-  cargo_nix = pkgs.callPackage ./Cargo.nix { inherit buildRustCrate pkgs; };
+  linuxBuildRustCrate = pkgs.buildRustCrate.override { rustc = rust; };
+  cargo_nix = pkgs.callPackage ./Cargo.nix {
+    inherit pkgs;
+    buildRustCrate = linuxBuildRustCrate;
+  };
   actionstar = cargo_nix.rootCrate.build;
   dockerimage = pkgs.dockerTools.buildLayeredImage {
     name = "actionstar";
