ef85ac7f5937f808c15c21170b83844a90dec149
Shane Sveller <shanesveller@gmail.com>
2020-03-01 09:10:29 -0600

    Use more succint syntax for compiling to musl targets

--- default.nix
+++ default.nix
@@ -2,10 +2,8 @@
 
 let
   # https://www.reddit.com/r/NixOS/comments/f0yi3b/how_to_build_a_simple_static_rust_binary_using/fh2asml/
-  rust = (pkgs.rustChannelOf { channel = "stable"; }).rust.override {
-    # extensions = [ "rust-src" ];
-    targets = [ "x86_64-unknown-linux-musl" ];
-  };
+  rust =
+    (pkgs.rustChannelOfTargets "stable" null [ "x86_64-unknown-linux-musl" ]);
   linuxBuildRustCrate = pkgs.buildRustCrate.override { rustc = rust; };
   cargo_nix = pkgs.callPackage ./Cargo.nix {
     inherit pkgs;
