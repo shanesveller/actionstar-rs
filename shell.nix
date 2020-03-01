b26f2ead8f45fb02e91be8657d1502fb1e95f9a3
Shane Sveller <shanesveller@gmail.com>
2020-03-01 10:45:58 -0600

    Install gnumake in shell.nix

--- shell.nix
+++ shell.nix
@@ -3,7 +3,7 @@
 with pkgs;
 
 mkShell {
-  buildInputs = [ crate2nix direnv lorri skopeo ]
+  buildInputs = [ crate2nix direnv gnumake lorri skopeo ]
     ++ (with pkgs.rustChannelOf { rustToolchain = ./rust-toolchain; };
       [ rust ]);
 }
