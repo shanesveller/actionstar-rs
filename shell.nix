b008965ac14d2c3e072bcc8d8e6ec5ef3d99f978
Shane Sveller <shanesveller@gmail.com>
2020-03-01 21:08:19 -0600

    Add missing OSX framework to shell.nix for compilation

--- shell.nix
+++ shell.nix
@@ -3,7 +3,7 @@
 with pkgs;
 
 mkShell {
-  buildInputs = [ crate2nix direnv gnumake lorri openssl pkgconfig skopeo ]
+  buildInputs = [ crate2nix darwin.apple_sdk.frameworks.Security direnv gnumake lorri openssl pkgconfig skopeo ]
     ++ (with pkgs.rustChannelOf { rustToolchain = ./rust-toolchain; };
       [ rust ]);
 }
