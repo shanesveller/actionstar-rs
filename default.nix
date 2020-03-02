6dc03eadebb9a0b62f2233bd677547e5616c2548
Shane Sveller <shanesveller@gmail.com>
2020-03-01 19:40:37 -0600

    Simplify back to one-platform default.nix

--- default.nix
+++ default.nix
@@ -1,30 +1,13 @@
 { system ? builtins.currentSystem, pkgs ? import ./nix { inherit system; } }:
 
 let
-  linuxPkgs = import ./nix { system = "x86_64-linux"; };
   # https://www.reddit.com/r/NixOS/comments/f0yi3b/how_to_build_a_simple_static_rust_binary_using/fh2asml/
-  rust = (linuxPkgs.rustChannelOfTargets "stable" null
-    [ "x86_64-unknown-linux-musl" ]);
-  linuxBuildRustCrate = linuxPkgs.buildRustCrate.override { rustc = rust; };
-  cargo_nix = linuxPkgs.callPackage ./Cargo.nix {
+  rust =
+    (pkgs.rustChannelOfTargets "stable" null [ "x86_64-unknown-linux-musl" ]);
+  linuxBuildRustCrate = pkgs.buildRustCrate.override { rustc = rust; };
+  cargo_nix = pkgs.callPackage ./Cargo.nix {
     buildRustCrate = linuxBuildRustCrate;
-    pkgs = linuxPkgs;
+    inherit pkgs;
   };
   actionstar = cargo_nix.rootCrate.build;
-  dockerimage = linuxPkgs.dockerTools.buildLayeredImage {
-    name = "actionstar";
-    tag = "latest";
-    contents = [ ];
-    config = { Cmd = "${actionstar}/bin/actionstar"; };
-  };
-
-  unpackedImage = pkgs.runCommand "unpacked-image" { } ''
-    ${pkgs.skopeo}/bin/skopeo --insecure-policy --override-os linux copy docker-archive:${dockerimage} dir:$out
-  '';
-
-  allImages = pkgs.linkFarm "images" [{
-    name = "actionstar";
-    path = unpackedImage;
-  }];
-
-in { inherit actionstar dockerimage unpackedImage allImages; }
+in { inherit actionstar; }
